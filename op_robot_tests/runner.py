76b6cdb91f0557e37e09f2264379c2a76113c805
OrysiaDrabych <orysia.drabych@quintagroup.org>
2018-04-13 17:58:21 +0300

    Fix buildout

--- op_robot_tests/runner.py
+++ op_robot_tests/runner.py
@@ -1,9 +1,9 @@
-from chromedriver import CHROMEDRV_PATH
+# from chromedriver import CHROMEDRV_PATH
 from robot import run_cli
 import os
 import sys
 
-sys.path.append(CHROMEDRV_PATH)
+# sys.path.append(CHROMEDRV_PATH)
 
 
 def runner():
